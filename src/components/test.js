4486096082d95d51555e636029875e889604e9bd
Junlong Wang <wjlinnz@gmail.com>
2017-11-19 00:00:42 +1300

    Modified to test fetching data from json-server and displaying them

--- src/components/test.js
+++ src/components/test.js
@@ -1,7 +1,51 @@
 import React from 'react';
 
-export default ({props}) =>{
-  return(
-    <h1>Hello World</h1>
-  )
+export default class Test extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {
+      boats: [],
+    };
+  }
+
+  componentDidMount () {
+    this._fetchData().then(res => {
+      this.setState({
+        boats: res
+      });
+    });
+  }
+
+  _fetchData () {
+    return fetch('http://localhost:3000/boats', {
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      else {
+        throw new Error(res.statusText);
+      }
+    });
+  }
+
+  render () {
+    const data = this.state.boats.map((boat, i) => {
+      return (
+        <li>{boat.name}</li>
+      );
+    });
+    return (
+      <p>
+        <h1>Hello World</h1>
+        <ul>
+          {data}
+        </ul>
+      </p>
+    );
+  }
 }
